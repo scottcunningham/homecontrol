b45be4d0d27679663aaa1c59d4e79c4cee50153c
Scott Cunningham <scottcunningham92@gmail.com>
2017-07-20 21:32:08 +0100

    serve everything from the app, inc. root html

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,7 @@ host=pi3
 src=$(echo $(dirname $0))/..
 srcs=$(echo $src/*.py $src/templates $src/static)
 initd=etc/init.d/volcontrol
+nginxvhost=etc/nginx/sites-enabled/default
 installdir=/home/pi/vol
 
 echo 'Deploying script..'
@@ -11,6 +12,9 @@ scp -r $srcs pi3:$installdir/
 scp -r $initd pi3:/tmp/vol-initd
 ssh -t pi3 sudo cp /tmp/vol-initd /etc/init.d/volcontrol
 ssh -t pi3 sudo systemctl daemon-reload
+scp -r $nginxvhost pi3:/tmp/vol-vhost
+ssh -t pi3 sudo cp /tmp/vol-vhost /etc/nginx/sites-enabled/default
+ssh -t pi3 sudo serive nginx reload
 echo 'Restarting service..'
 ssh -t pi3 sudo service volcontrol restart
 echo 'Checking service health..'
