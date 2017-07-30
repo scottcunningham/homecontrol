7e6a9cd60ce3eb7dccd0ae4a84f31baf458a6760
Scott Cunningham <scottcunningham92@gmail.com>
2017-07-30 14:55:42 +0100

    move fan/lamp to unified plug control

--- app.py
+++ app.py
@@ -3,9 +3,7 @@
 from flask import Flask, render_template
 
 from vol import vol_blueprint
-from fan import fan_blueprint
 from gui import gui_blueprint
-from lamp import lamp_blueprint
 from plugs import plugs_blueprint
 from tv import tv_blueprint
 
@@ -20,8 +18,6 @@ def root():
 
 if __name__ == '__main__':
     app.register_blueprint(vol_blueprint, url_prefix='/vol')
-    app.register_blueprint(fan_blueprint, url_prefix='/fan')
-    app.register_blueprint(lamp_blueprint, url_prefix='/lamp')
     app.register_blueprint(plugs_blueprint, url_prefix='/plugs')
     app.register_blueprint(gui_blueprint, url_prefix='/homecontrol')
     app.register_blueprint(tv_blueprint, url_prefix='/tv')
--- plugs.py
+++ plugs.py
@@ -1,11 +1,40 @@
 #!/usr/bin/env python3
 
+import json
 from flask import Blueprint
 from flask import render_template
+from pyHS100 import SmartPlug
 
 plugs_blueprint = Blueprint('plugs', __name__)
+
+plugs = {
+    'fan': {
+        'ip': '192.168.1.188',
+    },
+    'lamp': {
+        'ip': '192.168.1.190',
+    },
+}
 
 
 @plugs_blueprint.route('/')
 def view():
     return render_template('plugs.html')
+
+
+@plugs_blueprint.route('/toggle/<plug>')
+def plug_toggle(plug):
+    print("Toggling plug", plug)
+    p = SmartPlug(plugs[plug]['ip'])
+    if p.is_on:
+        p.turn_off()
+        return 'turned {} off'.format(plug)
+    else:
+        p.turn_on()
+        return 'turned {} on'.format(plug)
+
+
+@plugs_blueprint.route('/status/<plug>')
+def plug_status(plug):
+    p = SmartPlug(plugs[plug]['ip'])
+    return json.dumps({'status': p.is_on})
